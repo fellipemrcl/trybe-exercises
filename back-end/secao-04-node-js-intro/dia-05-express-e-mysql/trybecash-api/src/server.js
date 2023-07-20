4b3363823b6ab9d5e2c6c0660052ebb3a1f89d99
fellipemrcl <fmrclms@gmail.com>
2023-07-20 09:47:16 -0300

    exemplos finalizados

--- back-end/secao-04-node-js-intro/dia-05-express-e-mysql/trybecash-api/src/server.js
+++ back-end/secao-04-node-js-intro/dia-05-express-e-mysql/trybecash-api/src/server.js
@@ -4,4 +4,5 @@ const PORT = 3005;
 
 app.listen(PORT, () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
 });
